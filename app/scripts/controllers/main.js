1d2628df5dddab52aa64ee9c651a0254a43c82c3
juunp <judith.pivoteau@gmail.com>
2015-07-21 22:30:18 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amiiboListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amiiboListApp
+ */
+angular.module('amiiboListApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
